d46789d52dade176752712f1494e21e0c3bef44d
Nir Galili <nirgalili1@gmail.com>
2021-09-16 15:24:44 +0300

    add entry to txt file

--- main.py
+++ main.py
@@ -1,14 +1,35 @@
 from tkinter import *
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
+
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+# function for press add
+def press_add():
+
+    # open file data.txt
+
+    with open("data.txt", "a") as file:
+        # add new line to file
+        file.write(f"{input_website.get()} | {input_email.get()} | {input_password.get()}\n")
+
+    # clear field in UI
+
+    input_website.delete(0, END)
+    input_website.insert(0, "")
+
+    input_email.delete(0, END)
+    input_email.insert(0, "")
+
+    input_password.delete(0, END)
+    input_password.insert(0, "")
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 
 # window
 window = Tk()
 window.title("Password Manager")
-window.config(padx=20, pady=20, bg="white")
+window.config(padx=50, pady=50, bg="white")
 
 # canvas
 canvas = Canvas(width=200, height=200, bg="white", highlightthickness=0)
@@ -26,30 +47,25 @@ email.grid(column=0, row=2)
 password = Label(text="Password:", bg="white")
 password.grid(column=0, row=3)
 
-#Entry
-input_website = Entry(width=50)
+# Entry
+input_website = Entry(width=51)
 input_website.grid(column=1, row=1, columnspan=2)
+input_website.focus()
 
-input_email = Entry(width=50)
+input_email = Entry(width=51)
 input_email.grid(column=1, row=2, columnspan=2)
+input_email.insert(0, string="MyEmail@host.com")
 
 input_password = Entry(width=32)
 input_password.grid(column=1, row=3)
+
 
 # buttons
 generate_button = Button(text="Generate Password")
 generate_button.grid(column=2, row=3)
 
-add_button = Button(text="Add", width=43)
+add_button = Button(text="Add", width=43, command=press_add)
 add_button.grid(column=1, row=4, columnspan=2)
 
-# r = Label(bg="red", width=20, height=5)
-# r.grid(row=0, column=0)
-#
-# g = Label(bg="green", width=20, height=5)
-# g.grid(row=1, column=1)
-#
-# b = Label(bg="blue", width=40, height=5)
-# b.grid(row=2, column=0, columnspan=2)
 
 window.mainloop()
